32704b6c680099df9b8c67950db2ec97a09a9a57
Marin Cholakov <marinvch@gmail.com>
2021-03-19 13:44:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use(express.json());
 app.use(cookieParser());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(
