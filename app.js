eaa4efa322140dcfc6bbf12b5740494a9a5d4356
Marin Cholakov <marinvch@gmail.com>
2021-03-31 10:31:50 +0300

    dd

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ dotenv.config();
 
 const app = express();
 
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "http://forum.marinvch.eu"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -23,7 +30,6 @@ app.use("/comments", commentRoutes);
 //Setup server port
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
 //Connect to Databse
 mongoose
   .connect(process.env.MONGODB_URI, {
